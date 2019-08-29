6a6a253dba65ee0ab57d46a8482e1d77182be356
左龙 <544748770@qq.com>
2019-08-29 09:14:04 +0800

    1

---
+++ components/createWaterMark/createWaterMark.vue
@@ -0,0 +1,114 @@
+<template name="createWaterMark">
+	<view class="createWaterMark">
+		<view class="createWaterMark_content" :style="{'height':imgInfo.height+'px'}">
+			<canvas canvas-id="waterMark"></canvas>
+		</view>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				imgInfo:{},
+				screenW:""
+			}
+		},
+		props: ['imgUrl'],
+		created(){
+			// console.log(this.imgUrl)
+			this.getImgInfo(this.imgUrl)
+			this.screenW=uni.getSystemInfoSync().screenWidth
+		},
+		onShow() {
+			// console.log(this.imgUrl)
+		},
+		methods:{
+			getImgInfo(url){
+				uni.getImageInfo({
+				    src: url,
+				    success: (results)=> {
+				        // console.log(results.width);
+				        // console.log(results.height);
+				        // console.log(results.path);
+						
+						this.imgInfo={
+							path:results.path,
+							width:this.screenW,
+							height:results.height/results.width*this.screenW
+						}
+						this.drawMark(this.imgInfo)
+				    }
+				});
+			},
+			drawMark(data){
+				var ctx=uni.createCanvasContext("waterMark",this)
+				ctx.drawImage(data.path,0,0,data.width,data.height)
+				ctx.setFillStyle("#ff0000")
+				ctx.setFontSize(20)
+				ctx.fillText(this.getDate(),20,data.height-30)
+				ctx.draw()
+				uni.showLoading({})
+				setTimeout(this.getImg,1500)
+			},
+			getImg(){
+				uni.canvasToTempFilePath({
+				  x: 0,
+				  y: 0,
+				  width: this.width,
+				  height: this.imgInfo.height,
+				  destWidth: this.width,
+				  destHeight: this.imgInfo.height,
+				  canvasId: 'waterMark',
+				  success: (res)=> {
+				    // 在H5平台下，tempFilePath 为 base64
+				    // console.log(res.tempFilePath)
+					this.$emit("getPhotoUrl",res.tempFilePath)
+				  },
+				  fail(res){
+					  console.log(res)
+				  },
+				  complete(){
+					  uni.hideLoading()
+				  }
+				},this)
+			},
+			getDate(){
+				var date=new Date()
+				var y=date.getFullYear()
+				var m=date.getMonth()+1
+				var d=date.getDate()
+				var h=date.getHours()
+				var minute=date.getMinutes()
+				m=m<10?"0"+m:m
+				d=d<10?"0"+d:d
+				h=h<10?0+"h":h
+				minute=minute<10?"0"+minute:minute
+				return y+"-"+m+"-"+d+" "+h+":"+minute
+			}
+		}
+	}
+</script>
+
+<style scoped>
+.createWaterMark{
+	position: fixed;
+	width: 100vw;
+	height: 100vh;
+	background-color: #000000;
+	z-index: 10000;
+	top: 0rpx;
+	left: -800rpx;
+}
+.createWaterMark_content{
+	width: 100vw;
+	position: absolute;
+	top:0upx;
+	left: 0upx;
+}
+canvas{
+	width: 100%;
+	height: 100%;
+}
+</style>
---
+++ pages/showLocation/showLocation.vue
@@ -0,0 +1,212 @@
+<template>
+	<view id="location">
+		<map id="locationMap" style="width: 100%;height: 100vh;" :controls="controls" @controltap="bindControltap" :scale="20" :show-location="true" :markers="covers" :circles="circles" :latitude="latitude" :longitude="longitude">
+			<cover-view id="navigation"></cover-view>
+			<cover-view id="address">重庆市</cover-view>
+			<cover-view id="address_msg">{{pointInfo.address}}</cover-view>	
+			<cover-view id="distance">{{distance}}km</cover-view>
+		</map>
+	</view>
+</template>
+
+<script>
+	import URL from "../../static/js/interface.js"
+	import util from "../../static/js/utils.js"
+	import {mapTool} from '../../static/js/mapTool.js'
+	export default {
+		data() {
+			return {
+				covers:[],
+				circles:[],
+				latitude:"",
+				longitude:"",
+				taskId:"",
+				pointInfo:{},
+				userLocation:{},
+				distance:0,
+				controls:[]
+			}
+		},
+		onReady() {
+			var mapContext=uni.createMapContext("locationMap")
+			var locationMapInfo=mapContext.$getAppMap()
+			locationMapInfo.showUserLocation(true)
+			this.userInfo=uni.getStorageSync("userInfo")
+			this.pointInfo=util.pollutionInfo
+			this.userLocation=uni.getStorageSync("userLocation")
+			var screenWidth=uni.getSystemInfoSync().screenWidth
+			var windowHeight=uni.getSystemInfoSync().windowHeight
+			var coefficient=screenWidth/750
+			this.controls=[
+				{
+					id:"toNavigation",
+					position:{
+						width: 83*coefficient,
+						height: 83*coefficient,
+						left: 597*coefficient,
+						top:windowHeight-235*coefficient
+					},
+					iconPath:"/static/images/icon_navigation.png",
+					clickable:true
+				}
+			]
+			this.initMap(this.pointInfo)
+			this.getDistance()
+		},
+		onLoad(options){
+			
+		},
+		methods: {
+			initMap(data){
+				if(!data.longitude){
+					uni.showToast({
+						icon:"none",
+						title:"位置未记录，请重新定位！"
+					})
+					this.latitude=this.userLocation.latitude
+					this.longitude=this.userLocation.longitude
+				}else{
+					var wgs84togcj02=mapTool.wgs84togcj02(data.longitude,data.latitude)
+					this.longitude=wgs84togcj02[0]
+					this.latitude=wgs84togcj02[1]
+					this.circles=[
+						{
+							latitude: this.latitude,
+							longitude: this.longitude,
+							radius:data.distanceLimit,
+							color:"#5087FB80",
+							fillColor:"#5087FB50",
+							strokeWidth:"2"
+						}
+					]
+					this.covers=[{
+						id:"person1",
+						latitude: this.latitude,
+						longitude: this.longitude,
+						iconPath: '../../static/images/dingwei.png',
+						label:{
+							content:data.address,
+							color:"#5087FB"
+						}
+					}]
+				}
+			},
+			bindControltap(e){
+				if(e.controlId=="toNavigation"){
+					this.toNavigation()
+				}
+			},
+			getDistance(){
+				var point1 = new plus.maps.Point(this.userLocation.longitude,this.userLocation.latitude);
+				var point2 = new plus.maps.Point(this.pointInfo.longitude,this.pointInfo.latitude)
+				plus.maps.Map.calculateDistance(point1,point2,(res)=>{
+					this.distance=(res.distance/1000).toFixed(2)
+				})
+			},
+			toNavigation(){//导航
+				var _this = this;  
+				// 判断平台  
+				var wgs84togcj02=mapTool.wgs84togcj02(_this.pointInfo.longitude,_this.pointInfo.latitude)
+				var longitude=wgs84togcj02[0]
+				var latitude=wgs84togcj02[1]
+				if (plus.os.name == 'Android') {  
+					plus.runtime.openURL("amapuri://route/plan/?sid=BGVIS1&did=BGVIS2&dlat="+latitude+"&dlon="+longitude+"&dev=0&t=0",  
+						function(e) {  
+							console.log('Open system default browser failed: ' + e.message);  
+						},"com.autonavi.minimap"
+					);
+				} else if (plus.os.name == 'iOS') {  
+					plus.runtime.launchApplication({ action:"iosamap://path?sourceApplication=applicationName&sid=BGVIS1&did=BGVIS2&dlat="+latitude+"&dlon="+longitude+"&dev=0&t=0" }, function(e) {  
+						console.log('Open system default browser failed: ' + e.message);  
+					});  
+				}
+			},
+			toReport(){
+				var data={
+					id:this.pointInfo.pollutionId,
+					wryType:this.pointInfo.wryType,
+					longitude:this.userLocation.longitude,
+					latitude:this.userLocation.latitude,
+				}
+				uni.showModal({
+				    title: '提示',
+				    content: '确认重新记录位置？',
+				    success:(res)=> {
+				        if (res.confirm) {
+				            util.getRequest(URL.TASK_PATROL_POINT_UPDATE_WRY_ADDRESS_DETAILED,data,(results)=>{
+				            	console.log("成功")
+				            	this.pointInfo.longitude=this.userLocation.longitude
+				            	this.pointInfo.latitude=this.userLocation.latitude
+				            	this.initMap(this.pointInfo)
+				            	this.getDistance()
+				            })
+				        } else if (res.cancel) {
+				            console.log('用户点击取消');
+				        }
+				    }
+				});
+			}
+		}
+	}
+</script>
+
+<style scoped>
+#location{
+	height: 100vh;
+}
+#locationMap{
+	position: relative;
+}
+#reset{
+	width: 690upx;
+	height: 70upx;
+	position: absolute;
+	bottom: 30upx;
+	left: 30upx;
+	border-radius: 35upx;
+	background-color: #5087FB;
+	text-align: center;
+	line-height: 70upx;
+	color: #fff;
+	font-size: 32upx;
+}
+#navigation{
+	width: 690upx;
+	position: absolute;
+	bottom: 30upx;
+	left: 30upx;
+	border-radius: 10upx;
+	background-color: #fff;
+	text-align: center;
+	color: #333;
+	height: 129upx;
+	font-size: 32upx;
+}
+#address{
+	position: absolute;
+	bottom: 99upx;
+	left: 60upx;
+	font-size: 32upx;
+}
+#address_msg{
+	position: absolute;
+	bottom: 60upx;
+	left: 60upx;
+	width: 300px;
+	font-size: 24upx;
+}
+#distance{
+	position: absolute;
+	bottom: 79upx;
+	right: 60upx;
+	font-size: 32upx;
+}
+#toNavigation{
+	position: absolute;
+	bottom: 126upx;
+	right: 70upx;
+	border-radius: 50%;
+	width: 83upx;
+	height: 83upx;
+}
+</style>
